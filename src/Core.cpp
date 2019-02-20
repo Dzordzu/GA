33d7bcf54b6f7875215d7a9af62d7b82ffdf0627
Dzordzu <tomekdur@wp.pl>
2019-02-20 18:59:12 +0100

    Restarted

---
+++ src/Core.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dzordzu on 20.02.19.
+//
+
+#include "Core.hpp"
---
+++ src/Core.hpp
@@ -0,0 +1,14 @@
+//
+// Created by dzordzu on 20.02.19.
+//
+
+#ifndef GENETIC_ALGORITHM_CORE_HPP
+#define GENETIC_ALGORITHM_CORE_HPP
+
+
+class Core {
+
+};
+
+
+#endif //GENETIC_ALGORITHM_CORE_HPP
