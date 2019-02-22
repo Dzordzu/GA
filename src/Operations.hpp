d8b1f8ab0f22df634ba4ea13a381c19bc23a7aa8
Dzordzu <tomekdur@wp.pl>
2019-02-22 02:03:11 +0100

    Added simple operations interfaces

--- src/Operations.hpp
+++ src/Operations.hpp
@@ -5,10 +5,20 @@
 #ifndef GENETIC_ALGORITHM_OPERATIONS_HPP
 #define GENETIC_ALGORITHM_OPERATIONS_HPP
 
+#include "Core.hpp"
 
-class Operations {
+namespace GeneticAlgorithm {
+    namespace Operations {
 
-};
+        using GeneticAlgorithm::Core::Individual;
+
+        class Mutation {public: virtual void mutate(Individual &individual) = 0;};
+        class Crossover {
+        public:
+            virtual void cross(Individual &target, Individual source) = 0;
+        };
+    }
+}
 
 
 #endif //GENETIC_ALGORITHM_OPERATIONS_HPP
