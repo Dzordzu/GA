272057d7601d72dbe8adf9044640d5520f9da25f
Dzordzu <tomekdur@wp.pl>
2019-02-22 01:27:00 +0100

    Added Operations file

---
+++ src/Operations.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dzordzu on 22.02.19.
+//
+
+#include "Operations.hpp"
---
+++ src/Operations.hpp
@@ -0,0 +1,14 @@
+//
+// Created by dzordzu on 22.02.19.
+//
+
+#ifndef GENETIC_ALGORITHM_OPERATIONS_HPP
+#define GENETIC_ALGORITHM_OPERATIONS_HPP
+
+
+class Operations {
+
+};
+
+
+#endif //GENETIC_ALGORITHM_OPERATIONS_HPP
